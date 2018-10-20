2e514da35df670ad9510455fab25e9fac30662b4
melton3413 <melton3413@gmail.com>
2018-10-20 15:28:50 -0400

    1st commit

---
+++ app/data/friends.js
@@ -0,0 +1,2 @@
+// Includes a single default profile, so that anyone demoing from the GitHub repository will have a result.
+module.exports = [{"name":"Melton","photo":"https://media.giphy.com/media/9AINKf3qYtnvC5yHvk/giphy.gif","answers":["2","5","5","4","5","1","1","2","2","2"]}];
